9a6a90a699ef721d14caa8f1b48a7e0984c93638
namgigun <84756980+namgigun@users.noreply.github.com>
2025-08-28 02:43:25 +0900

    refactor(be): 댓글 엔티티 변경 (#1) (#5)

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -86,6 +86,12 @@ kotlin {
     }
 }
 
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
 tasks.withType<Test> {
     useJUnitPlatform()
 }
