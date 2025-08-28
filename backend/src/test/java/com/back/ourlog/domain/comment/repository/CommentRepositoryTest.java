0fe941e9cd84dfb29d89e2b5a7a50bd07a04a001
namgigun <namgigun@gmail.com>
2025-08-28 16:39:26 +0900

    Fix: Build.gradle 의존성 삭제
    - 코틀린 JUNIT 관련 설정 삭제

--- backend/src/test/java/com/back/ourlog/domain/comment/repository/CommentRepositoryTest.java
+++ backend/src/test/java/com/back/ourlog/domain/comment/repository/CommentRepositoryTest.java
@@ -4,6 +4,7 @@ import com.back.ourlog.domain.comment.entity.Comment;
 import com.back.ourlog.domain.diary.entity.Diary;
 import com.back.ourlog.domain.diary.repository.DiaryRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,11 +17,12 @@ import java.util.List;
 public class CommentRepositoryTest {
     @Autowired
     private DiaryRepository diaryRepository;
-
     @Autowired
     private CommentRepository commentRepository;
+
     @Test
-    @Transactional
+    @DisplayName("댓글 최신 순으로 나열")
+    @Transactional(readOnly = true)
     void t1() {
         Diary diary = diaryRepository.findById(1).get();
 
