f76432e2a1cefe7bd2c201cd05c10af512211cf0
namgigun <namgigun@gmail.com>
2025-08-28 16:39:26 +0900

    Fix: Build.gradle 의존성 삭제
    - 코틀린 JUNIT 관련 설정 삭제

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -82,7 +82,6 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.12.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.12.2")
     testImplementation("org.springframework.security:spring-security-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 }
 
 kotlin {
