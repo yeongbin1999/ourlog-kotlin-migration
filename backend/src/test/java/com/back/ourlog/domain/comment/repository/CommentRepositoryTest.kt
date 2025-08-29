ad9d754e5850e681b95b7dac94687cbfc458c320
namgigun <namgigun@gmail.com>
2025-08-29 16:27:02 +0900

    Fix: 댓글 최신 순 나열 메서드 구현체 수정
    - 기존 댓글의 다이어리 ID와 매개변수의 ID를 비교  -> 변경 댓글의 다이어리 객체와 매개변수의 다이어리 객체의 일치 여부 비교

--- backend/src/test/java/com/back/ourlog/domain/comment/repository/CommentRepositoryTest.kt
+++ backend/src/test/java/com/back/ourlog/domain/comment/repository/CommentRepositoryTest.kt
@@ -6,25 +6,23 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest
 @ActiveProfiles("test")
-class CommentRepositoryTest {
-    @Autowired
-    private val diaryRepository: DiaryRepository? = null
-
-    @Autowired
-    private val commentRepository: CommentRepository? = null
-
+class CommentRepositoryTest @Autowired constructor(
+    private val diaryRepository: DiaryRepository,
+    private val commentRepository: CommentRepository,
+) {
     @Test
     @DisplayName("댓글 최신 순으로 나열")
     @Transactional(readOnly = true)
     fun t1() {
-        val diary = diaryRepository!!.findById(1).get()
+        val diary = diaryRepository.findByIdOrNull(1)
 
-        val comments = commentRepository!!.findQByDiaryOrderByCreatedAtDesc(diary)
+        val comments = commentRepository.findQByDiaryOrderByCreatedAtDesc(diary!!)
 
         Assertions.assertThat(comments[0].createdAt).isAfter(comments[1].createdAt)
     }
