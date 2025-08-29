e8a69c71bae021f9eee4f363d8027bab7e997862
namgigun <namgigun@gmail.com>
2025-08-29 11:19:42 +0900

    Refactor: 댓글 서비스 코틀린 전환
    - getComments 댓글 정보 조회 메서드 변경
    기존 findBy... (JPQL) -> 변경 findQBy (QueryDSL)
    
    - 댓글 서비스 코드 코틀린 전환

---
+++ backend/src/main/java/com/back/ourlog/domain/comment/service/CommentService.kt
@@ -0,0 +1,84 @@
+package com.back.ourlog.domain.comment.service
+
+import com.back.ourlog.domain.comment.dto.CommentResponseDto
+import com.back.ourlog.domain.comment.repository.CommentRepository
+import com.back.ourlog.domain.diary.repository.DiaryRepository
+import com.back.ourlog.domain.user.entity.User
+import com.back.ourlog.global.exception.CustomException
+import com.back.ourlog.global.exception.ErrorCode
+import lombok.RequiredArgsConstructor
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@RequiredArgsConstructor
+class CommentService(
+    private val diaryRepository: DiaryRepository,
+    private val commentRepository: CommentRepository,
+) {
+    @Transactional
+    fun write(diaryId: Int, user: User?, content: String): CommentResponseDto {
+        val user = user ?: throw CustomException(ErrorCode.USER_NOT_FOUND)
+
+        val diary = diaryRepository.findByIdOrNull(diaryId)
+            ?: throw CustomException(ErrorCode.DIARY_NOT_FOUND)
+
+        val comment = diary.addComment(user, content)
+        // 아이디 값을 넣기 위해 사용
+        diaryRepository.flush()
+
+        return CommentResponseDto(comment)
+    }
+
+    @Transactional(readOnly = true)
+    fun getComments(diaryId: Int): List<CommentResponseDto> {
+        val diary = diaryRepository.findByIdOrNull(diaryId)
+            ?: throw CustomException(ErrorCode.DIARY_NOT_FOUND)
+
+        // 최신 순으로 나열된 댓글 정보
+        return commentRepository.findQByDiaryOrderByCreatedAtDesc(diary)
+            .map { CommentResponseDto(it) }
+    }
+
+    @Transactional
+    fun update(id: Int, content: String) {
+        val comment = commentRepository.findByIdOrNull(id)
+            ?: throw CustomException(ErrorCode.COMMENT_NOT_FOUND)
+
+        comment.update(content)
+    }
+
+    @Transactional
+    fun delete(id: Int) {
+        val comment = commentRepository.findByIdOrNull(id)
+            ?: throw CustomException(ErrorCode.COMMENT_NOT_FOUND)
+
+        // (Diary, User) 와 Comment 연관관계 제거
+        comment.diary.deleteComment(comment)
+
+        comment.user.deleteComment(comment)
+    }
+
+    @Transactional(readOnly = true)
+    fun checkCanDelete(user: User?, commentId: Int) {
+        val user = user ?: throw CustomException(ErrorCode.USER_NOT_FOUND)
+
+        val comment = commentRepository.findByIdOrNull(commentId)
+            ?: throw CustomException(ErrorCode.COMMENT_NOT_FOUND)
+
+        comment.user.takeIf { it == user }
+            ?: throw CustomException(ErrorCode.COMMENT_DELETE_FORBIDDEN)
+    }
+
+    @Transactional(readOnly = true)
+    fun checkCanUpdate(user: User?, commentId: Int) {
+        val user = user ?: throw CustomException(ErrorCode.USER_NOT_FOUND)
+
+        val comment = commentRepository.findByIdOrNull(commentId)
+            ?: throw CustomException(ErrorCode.COMMENT_NOT_FOUND)
+
+        comment.user.takeIf { it == user }
+            ?: throw CustomException(ErrorCode.COMMENT_UPDATE_FORBIDDEN)
+    }
+}
