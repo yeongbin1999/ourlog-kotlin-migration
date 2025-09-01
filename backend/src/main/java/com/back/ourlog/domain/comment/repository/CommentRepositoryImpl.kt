cd2b786db2be538f7f8e55945dd195c3f2383f75
namgigun <84756980+namgigun@users.noreply.github.com>
2025-09-01 15:34:52 +0900

    refactor(be): 중복코드 수정 (#52) (#56)
    * Refactor: 중복된 예외처리 로직 확장함수로 분리
    
    - 엔티티 데이터 NULL 처리 함수 추가 -> getOrThrow()
    - 리포지토리 ID 조회 시, NULL 처리 함수 추가 -> findByIdOrNull(ID, ErrorCode)
    
    * Fix: N + 1 문제 해결
    
    - Comment에서 User 객체를 호출할 때마다 추가 쿼리 발생
    
    - findQByDiaryOrderByCreatedAtDesc에서 fetch join을 사용하여 해결
    
    * Refactor: 댓글 삭제, 수정 로직 변경
    
    - 기존: 삭제가능 여부 로직과 수정가능 여부 로직을 따로 분리 -> 변경: 접근 가능 여부 로직을 하나를 만들고 erroCode로 삭제 가능인지 수정 가능인지 구분
    
    - 기존 : 컨트롤러에서 접근 가능 여부 로직과 삭제 로직을 분리 -> 변경: 삭제 로직안에 접근 가능 여부 로직을 사용하는 방식으로 변경
    
    * Fix: 댓글 정보를 최신 순으로 나열하는 쿼리 메서드 변경
    
    - 기존: Diary를 파라미터로 사용 -> 변경: DiaryId를 파라미터로 사용
    
    * Refactor: 댓글 조회 로직 변경
    
    - 기존
    1. DiaryId로 조회 후, Diary를 조회
    2. Id에 해당하는 Diary가 없는 경우 에러 발생
    2. 조회한 Diary로 Diary에 해당하는 댓글을 조회
    
    - 변경
    1. DiaryId로 Diary의 댓글을 조회
    2. 댓글이 없는 경우에 DiaryRepository에서 DiaryId에 해당하는 Diary 데이터가 있는지 검사하고 없는 경우 에러발생
    
    - 개선점
    기존 방식 -> 무조건 쿼리 2개(Diary 조회, Diary에 해당하는 댓글 조회) 호출,
    변경한 방식 -> 댓글이 없는 경우만 2개의 쿼리(DiaryId에 해당하는 댓글 조회, 다이어리 존재 여부 확인) 호출
    	       댓글이 있는 경우는 1개의 쿼리(DiaryId에 해당하는 댓글 조회)만 호출

--- backend/src/main/java/com/back/ourlog/domain/comment/repository/CommentRepositoryImpl.kt
+++ backend/src/main/java/com/back/ourlog/domain/comment/repository/CommentRepositoryImpl.kt
@@ -2,18 +2,20 @@ package com.back.ourlog.domain.comment.repository
 
 import com.back.ourlog.domain.comment.entity.Comment
 import com.back.ourlog.domain.comment.entity.QComment
-import com.back.ourlog.domain.diary.entity.Diary
+import com.back.ourlog.domain.user.entity.QUser
 import com.querydsl.jpa.impl.JPAQueryFactory
 
 class CommentRepositoryImpl(
     private val queryFactory: JPAQueryFactory
 ) : CommentRepositoryCustom {
-    override fun findQByDiaryOrderByCreatedAtDesc(diary: Diary): List<Comment> {
+    override fun findQByDiaryIdOrderByCreatedAtDesc(diaryId: Int): List<Comment> {
         val comment = QComment.comment
+        val user = QUser.user
 
         return queryFactory
             .selectFrom(comment)
-            .where(comment.diary.eq(diary))
+            .join(comment.user, user).fetchJoin()
+            .where(comment.diary.id.eq(diaryId))
             .orderBy(comment.createdAt.desc())
             .fetch()
     }
