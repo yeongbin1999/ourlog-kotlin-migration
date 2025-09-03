0dbaaad0111bd4d198476a41326d43bdff562fab
yeongbin1999 <yeongbin1999@gmail.com>
2025-09-03 22:35:29 +0900

    refactor(be): 무조건 초기 더미 데이터 생성

--- backend/src/main/java/com/back/ourlog/domain/auth/controller/AuthController.kt
+++ backend/src/main/java/com/back/ourlog/domain/auth/controller/AuthController.kt
@@ -13,7 +13,6 @@ import jakarta.servlet.http.HttpServletResponse
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseCookie
 import org.springframework.http.ResponseEntity
-import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 
 @RestController
